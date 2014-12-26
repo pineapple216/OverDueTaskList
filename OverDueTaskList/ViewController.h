68b445dfeacc584af299051b8d81d28d63100704
Koen Hendriks <koenhendriks89@gmail.com>
2014-12-26 14:02:11 +0100

    Initial Commit

---
+++ OverDueTaskList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OverDueTaskList
+//
+//  Created by Koen Hendriks on 26/12/14.
+//  Copyright (c) 2014 Koen Hendriks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OverDueTaskList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OverDueTaskList
+//
+//  Created by Koen Hendriks on 26/12/14.
+//  Copyright (c) 2014 Koen Hendriks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
